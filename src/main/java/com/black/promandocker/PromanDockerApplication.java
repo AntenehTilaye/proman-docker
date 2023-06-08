b2e75aeea8c0f9acbf469eeaad2c4481b50573d0
Anteneh Tilaye <antenehtilaye19@gmail.com>
2023-06-08 10:52:59 +0300

    init: app

---
+++ src/main/java/com/black/promandocker/PromanDockerApplication.java
@@ -0,0 +1,13 @@
+package com.black.promandocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromanDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromanDockerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/black/promandocker/PromanDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.black.promandocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PromanDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
